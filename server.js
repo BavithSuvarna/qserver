3ca1a11f8a62c4250c75f207823a89050bc54a6f
Bavith <suvarnabavith@gmail.com>
2025-06-19 10:42:24 +0530

    server.js updated

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB error:', err));
 
 app.use('/api/queue', require('./routes/queue'));
